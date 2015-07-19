2ee02f717a57b97ded63fc8e2b8d305af2e88aa6
Jay Mo <unitedjaymo@live.com>
2015-07-19 11:30:34 -0400

    Mithril Sidebar

--- pigeon/src/main/resources/js/main.js
+++ pigeon/src/main/resources/js/main.js
@@ -47,7 +47,11 @@ TiwiTalk.view = function(ctrl) {
     }
     showOpt = m("div.messenger-container", [
       // m("p", "Hello, " + ctrl.userInfo.name + "!"),
+      
+      
       m("div#messenger", [
+        
+        /* Left Bar */
         m("div.tiwi", [
           m("div.friend-preview", [
             m("div.face-container", [
@@ -72,6 +76,35 @@ TiwiTalk.view = function(ctrl) {
           ]),
           // m("button#logout", { onclick: ctrl.logout.bind(ctrl) }, "Logout")
         ]),
+        
+        /* Preview Bar - Right Side */
+        m("div.preview", [
+          m("div.search", [
+            m("form", [
+              m("input.search-friend", {
+                type: "text", placeholder: "Search"  
+              }),  
+            ]),  
+          ]),
+          m("div.friend-preview", [
+            m("div.face-container", [
+              m("div.vertical-container", [
+                m("div.vertical-align", [
+                  m("img.face")  
+                ]),  
+              ]),  
+            ]),
+            m("div.show", [
+              m("div.vertical-container", [
+                m("div.vertical-align", [
+                  m("h1.friend-name"),
+                  m("h2.excerpt")  
+                ]),  
+              ]),  
+            ]),  
+          ]),
+        ]),
+        
         m("div#sidebar", [
           m("input[placeholder=Enter ID Here]", {
             type: "text", oninput: m.withAttr("value", ctrl.inviteField)
