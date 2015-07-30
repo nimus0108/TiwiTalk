54c060231ae8cd99e0e5d6838f912060a482e00e
nimus0108 <nimus0108@gmail.com>
2015-07-30 14:36:38 -0400

    deleted useless files

--- TiwiTalk/TiwiTalk/TiwiTalk-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <JSQMessagesViewController/JSQMessages.h>
-#import <JSQMessagesViewController/UIColor+JSQMessages.h>
-#import <JSQMessagesViewController/JSQMessageData.h>
-#import <SocketRocket/SRWebSocket.h>
-
