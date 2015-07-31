d75053dd327e9a6d5e2c2214abaf31883d63c13e
Technius <techniux@gmail.com>
2015-07-31 14:36:32 -0400

    Fix assets and web pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,7 @@ import java.time.Clock
 import javax.inject.Inject
 import play.api._
 import play.api.data.Form
-import play.api.data.Forms.{ mapping, nonEmptyText, optional, text }
+import play.api.data.Forms.{ mapping, nonEmptyText, optional, single, text }
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.libs.json._
 import play.api.libs.concurrent.Execution.Implicits._
@@ -22,6 +22,8 @@ class Application @Inject()(users: UsersDAO, tokens: TokensDAO,
     mailer: MailerClient, val messagesApi: MessagesApi) extends Controller
     with I18nSupport {
 
+  val beginSignupForm = Form(single("email" -> nonEmptyText))
+
   val verifyForm = Form(
     mapping(
       "token" -> nonEmptyText,
@@ -31,7 +33,7 @@ class Application @Inject()(users: UsersDAO, tokens: TokensDAO,
   )
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
   def all = Action.async {
@@ -41,38 +43,47 @@ class Application @Inject()(users: UsersDAO, tokens: TokensDAO,
     }
   }
 
-  def sendVerificationEmail(email: String) = Action.async {
-    val token = UUID.randomUUID
-    val time = Clock.systemUTC.millis
-    val fut = for {
-      user <- users.find(email) if !user.isDefined
-      _ <- tokens.deleteEmailToken(email)
-      _ <- tokens.insert(Token(email, token, time))
-    } yield {
-      val bodyText = views.html.verificationEmail(email, token.toString).body
-      val mailToSend = Email(
-        "Complete Your Registration For TiwiTalk!",
-        "No-reply <noreply@tiwitalk.com>",
-        Seq("<" + email + ">"),
-        bodyHtml = Some(bodyText))
-      mailer.send(mailToSend)
-      Ok(Json.obj())
-    }
+  def sendVerificationEmail = Action.async { implicit request =>
+    beginSignupForm.bindFromRequest.fold(
+      error => Future.successful(BadRequest("")),
+      email => {
+        val token = UUID.randomUUID
+        val time = Clock.systemUTC.millis
+        val fut = for {
+          user <- users.find(email) if !user.isDefined
+          _ <- tokens.deleteEmailToken(email)
+          _ <- tokens.insert(Token(email, token, time))
+        } yield {
+          val bodyText = views.html.verificationEmail(email, token.toString).body
+          val mailToSend = Email(
+            "Complete Your Registration For TiwiTalk!",
+            "No-reply <noreply@tiwitalk.com>",
+            Seq("<" + email + ">"),
+            bodyHtml = Some(bodyText))
+          mailer.send(mailToSend)
+          Ok(Json.obj())
+        }
 
-    fut recover {
-      case _: NoSuchElementException => Ok(Json.obj())
-    }
+        fut recover {
+          case _: NoSuchElementException => Ok(Json.obj())
+        }
+      }
+    )
   }
 
-  def registerPage(token: String) = Action.async { implicit request =>
-    // TODO: Do this properly
-    @inline def invalid = NotFound(Json.obj("valid" -> false))
-    Try(UUID.fromString(token)) map { uuid =>
+  def registerPage = Action.async { implicit request =>
+    @inline def invalid = Redirect("/")
+    val response = for {
+      token <- request.queryString.get("token") flatMap (_.headOption)
+      uuid <- Try(UUID.fromString(token)).toOption
+    } yield {
       tokens.find(uuid) map {
-        case Some(_) => Ok(Json.obj("valid" -> true))
+        case Some(_) => Ok(views.html.register(uuid.toString))
         case None => invalid
       }
-    } getOrElse Future.successful(invalid)
+    }
+    
+    response getOrElse Future.successful(invalid)
   }
 
   def register = Action.async { implicit request =>
